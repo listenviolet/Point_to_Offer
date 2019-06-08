27894612c6d9910c2f990035ab0a7a709499e59d
listenviolet <listenviolet@gmail.com>
2019-06-08 09:06:53 +0800

    use bit op to do add

--- 47_imple_add_not_use_+_.cpp
+++ 47_imple_add_not_use_+_.cpp
@@ -15,3 +15,15 @@ public:
         return num1;
     }
 };
+
+// 写一个函数，求两个整数之和，
+// 要求在函数体内不得使用+、-、*、/四则运算符号。
+
+// 运行时间：4ms
+// 占用内存：480k
+
+// Notice:
+// return num1 not Sum;
+// when num2 != 0, it will be in while, the sum and num1 are same
+// when num2 == 0, it won't be in while, or num1 keep the last in while sum.
+// eg. input 3, 0
