675290e35bddea1f79960f2c9c4f8109bac079fc
listenviolet <listenviolet@gmail.com>
2019-06-11 09:45:48 +0800

    multiply array without i - not using divide - triangle

---
+++ 52_multiply_not_use_div-AB-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> multiply(const vector<int>& A) {
+        int length = A.size();
+        if(length == 0) {vector<int> B; return B;}
+        vector<int> B(length);
+        B[0] = 1;
+        for(int i = 1; i < length; ++i)
+        {
+            B[i] = B[i - 1] * A[i - 1];
+        }
+        
+        double tmp = 1;
+        for(int i = length - 2; i >= 0; --i)
+        {
+            tmp *= A[i + 1];
+            B[i] *= tmp;
+        }
+        return B;
+        
+    }
+};
+
+// 给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],
+// 其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。不能使用除法。
+
+// 运行时间：7ms
+// 占用内存：460k
